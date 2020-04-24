73854d3745254a778795b25ed30a15838dc6ef8d
Adriano <adriano.selaviles@gmail.com>
2020-04-24 14:19:56 -0700

    adjust interval

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func smurfHandler(ctx *cli.Context) error {
 		return err
 	}
 
-	interval := time.Millisecond * 1000
+	interval := time.Millisecond * 10
 	fmt.Printf(banner, victim, proxy, interval.String(), hex.Dump(p.payload))
 	for {
 		if err = p.execute(); err != nil {
