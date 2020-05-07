17efc0685e3b56183ef0f641ca597629a8cdaf65
Adriano <adriano.selaviles@gmail.com>
2020-05-07 16:22:47 -0700

    using only fmt and os in main

--- app/app.go
+++ app/app.go
@@ -6,37 +6,16 @@ import (
 	cli "gopkg.in/urfave/cli.v1"
 )
 
-const (
-	banner = `
-             github.com/adrianosela/     $$$$$$\
-                                        $$  __$$\
- $$$$$$$\  $$$$$$\   $$$$$$\   $$$$$$\  $$ /  \__|
-$$  _____|$$  __$$\ $$  __$$\ $$  __$$\ $$$$\
-\$$$$$$\  $$ /  $$ |$$ /  $$ |$$ /  $$ |$$  _|
- \____$$\ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |
-$$$$$$$  |$$$$$$$  |\$$$$$$  |\$$$$$$  |$$ |
-\_______/ $$  ____/  \______/  \______/ \__|
-          $$ |
-          $$ |
-          \__|
-`
-	usage = "a utility for injecting spoofed frames into a network"
-)
-
 // Run runs the spoof cli
 func Run(version string, args []string) error {
 	app := cli.NewApp()
-
 	app.Version = version
-	app.Usage = usage
+	app.Usage = "a utility for injecting spoofed frames into a network"
 	app.Commands = commands
 	app.EnableBashCompletion = true
 	app.CommandNotFound = func(c *cli.Context, cmd string) {
 		c.App.Run([]string{"help"})
 		fmt.Printf("\ncommand \"%s\" does not exist\n", cmd)
 	}
-
-	fmt.Println(banner)
-
 	return app.Run(args)
 }
--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/adrianosela/spoof/app"
 )
 
+const banner = `
+             github.com/adrianosela/     $$$$$$\
+                                        $$  __$$\
+ $$$$$$$\  $$$$$$\   $$$$$$\   $$$$$$\  $$ /  \__|
+$$  _____|$$  __$$\ $$  __$$\ $$  __$$\ $$$$\
+\$$$$$$\  $$ /  $$ |$$ /  $$ |$$ /  $$ |$$  _|
+ \____$$\ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |
+$$$$$$$  |$$$$$$$  |\$$$$$$  |\$$$$$$  |$$ |
+\_______/ $$  ____/  \______/  \______/ \__|
+          $$ |
+          $$ |
+          \__|
+`
+
 var version string // injected at build-time
 
 func main() {
+	fmt.Println(banner)
 	if err := app.Run(version, os.Args); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
